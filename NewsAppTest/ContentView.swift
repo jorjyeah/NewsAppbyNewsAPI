7a5567d2c90d8d30b0a47496e5721278bea346e0
George Joseph Kristian <jorji.gjk@gmail.com>
2020-05-06 23:23:40 +0700

    commit

--- NewsAppTest/ContentView.swift
+++ NewsAppTest/ContentView.swift
@@ -7,15 +7,60 @@
 //
 
 import SwiftUI
+import SDWebImage
+import SDWebImageSwiftUI
 
 struct ContentView: View {
+    @ObservedObject var newsFeed = NewsFeed()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            List(newsFeed) {(article: NewsListItem) in
+                NewsListItemView(article: article)
+                    .onAppear{
+                        self.newsFeed.loadMoreArticles(currentItem: article)
+                }
+            }.navigationBarTitle("News")
+        }
+        
+    }
+//    var body: some View {
+//        NewsFeedView()
+//    }
+}
+
+
+struct NewsListItemView: View {
+    var article: NewsListItem
+    
+    var body: some View{
+        NavigationLink(destination: webView(item: article)){
+            HStack(spacing: 10){
+                
+                if article.image != ""{
+                    WebImage(url: URL(string: article.image), options: .highPriority, context: nil)
+                    .resizable()
+                    .frame(width: 100, height: 80)
+                    .cornerRadius(6)
+                    .scaledToFill()
+                    .padding()
+                }
+                
+                VStack(alignment: .leading){
+                Text("\(article.title)").font(.headline)
+                Text("\(article.author)").font(.subheadline)
+                }
+        }
+        .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+//        NavigationView{
+            ContentView()
+//            .navigationBarTitle("News")
+//        }
     }
 }
